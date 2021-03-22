1aabdb2081bb5fa3dda8333fb2d94f999ce8516d
ranha <dev.ranha@gmail.com>
2021-03-22 17:38:17 +0900

    initialize

---
+++ generator.rb
@@ -0,0 +1,26 @@
+require "bibtex"
+
+dir_name = ARGV[0] # YYYY_MM
+match = /(....)_(..)/.match(dir_name)
+year = match[1]
+month = match[2]
+bib_file_name = "#{dir_name}/books.bib"
+md_file_name = "#{dir_name}/books.md"
+
+def md_entry_str(info)
+  body = "\n\n"
+  body += "# #{info[:title]}\n"
+  body += "* 著者: #{info[:author].to_s}\n"
+  body += "* 出版社: #{info[:publisher].to_s}\n"
+  body += "* 書籍ページ: #{info[:url].to_s}\n"
+end
+
+bib = BibTeX.open(bib_file_name)
+
+File.open(md_file_name, "w") {|file|
+  file.puts("#{year}年の#{month}に出る本のページ\n")
+  bib.each_entry {|info|
+    s = md_entry_str(info)
+    file.puts(s)
+  }
+}
